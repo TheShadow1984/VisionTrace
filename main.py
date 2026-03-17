e09210461004c1897d85e69397980c795bcfb8eb
Shadow <shadows.mail@icloud.com>
2026-03-17 17:12:34 +0100

    Recompiled logic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-host = requests.get("https://internetdb.shodan.io/1.1.1.1").json()
+host = requests.get("https://internetdb.shodan.io/8.8.8.8").json()
 
 with open("results.json", "w", encoding="utf-8") as file:
     json.dump(host, file, ensure_ascii=False, indent=4)
